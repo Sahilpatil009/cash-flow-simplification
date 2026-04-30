41f1785eeaf695cc9c38b8ea3540183a7ebb7ca4
sahil patil <173548370+Sahilpatil009@users.noreply.github.com>
2026-04-30 12:25:24 +0530

    deployment to vercel

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Helper: Generate C++ input format
 function generateCppInput(transactions) {
@@ -200,6 +200,14 @@ app.post('/api/algorithms/all', (req, res) => {
 // Test endpoint
 app.get('/api/test', (req, res) => {
     res.json({ message: 'Server is running!', timestamp: new Date() });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 if (require.main === module) {
